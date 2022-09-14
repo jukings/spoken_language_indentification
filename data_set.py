4f5f32900e65b297d593992ae4be151ccf727572
jukings <salvador.julien@outlook.com>
2022-09-14 20:11:10 +0200

    little corrections data_set.py

--- data_set.py
+++ data_set.py
@@ -4,20 +4,20 @@ import os
 import librosa
 import numpy as np
 
-ROOT_DIR = './data'
+DATA_DIR = './data'
 DIC_VALUE_TO_LABEL = {0 : 'en', 1 : 'de', 2 : 'es'}
 DIC_LABEL_TO_VALUE = {'en' : 0, 'de' : 1, 'es' : 2}
 
 class SpokenLanguageIdentification(Dataset):
     
-    def __init__(self, root_dir, train=True, transform=None):
+    def __init__(self, data_dir, train=True, transform=None):
         
         if train is True :
-          self.root_dir = f'{root_dir}/train/train'
-          self.path_list = os.listdir(self.root_dir)
+          self.data_dir = f'{data_dir}/train/train'
+          self.path_list = os.listdir(self.data_dir)
         else :
-          self.root_dir = f'{root_dir}/test/test'
-          self.path_list = os.listdir(self.root_dir)
+          self.data_dir = f'{data_dir}/test/test'
+          self.path_list = os.listdir(self.data_dir)
 
         self.transform = transform
 
@@ -28,7 +28,7 @@ class SpokenLanguageIdentification(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-        audio = librosa.load(f'{self.root_dir}/{self.path_list[idx]}')
+        audio = librosa.load(f'{self.data_dir}/{self.path_list[idx]}')
         label = self.path_list[idx][:2]
         tmp = np.zeros(3)
         tmp[int(DIC_LABEL_TO_VALUE[label])]=1
