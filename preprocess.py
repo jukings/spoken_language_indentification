b60d81db28b198e34cd35e5498e05eaa0e47b13e
jukings <salvador.julien@outlook.com>
2022-09-22 16:56:28 +0200

    adding new data dir in args

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,9 @@ import numpy as np
 import argparse
 from tqdm import tqdm
 DATA_DIR = './data'
+NEW_DATA_DIR = './data'
 
-
-def build_mfccs(data_dir = DATA_DIR, train=True) :
+def build_mfccs(data_dir = DATA_DIR, new_data_dir = NEW_DATA_DIR, train=True) :
 
     if train :
         t = tqdm(os.listdir(f'{DATA_DIR}/train/train_audio/'))
@@ -16,7 +16,7 @@ def build_mfccs(data_dir = DATA_DIR, train=True) :
             signal, sr = librosa.load(f'{DATA_DIR}/train/train_audio/{file}')
             mfccs = librosa.feature.mfcc(y=signal, n_mfcc=10,sr=sr)
             new_file = file[:-4]
-            np.save(f'{DATA_DIR}/train/train_mfccs/{new_file}mfccs',mfccs)
+            np.save(f'{NEW_DATA_DIR}/train/train_mfccs/{new_file}mfccs',mfccs)
 
     if not train :
         t = tqdm(os.listdir(f'{DATA_DIR}/test/test_audio/'))
@@ -25,15 +25,16 @@ def build_mfccs(data_dir = DATA_DIR, train=True) :
             signal, sr = librosa.load(f'{DATA_DIR}/test/test_audio/{file}')
             mfccs = librosa.feature.mfcc(y=signal, n_mfcc=10,sr=sr)
             new_file = file[:-4]
-            np.save(f'{DATA_DIR}/test/test_mfccs/{new_file}mfccs',mfccs)
+            np.save(f'{NEW_DATA_DIR}/test/test_mfccs/{new_file}mfccs',mfccs)
 
 if __name__=='__main__' :
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default = './data', help='directory where to find the data')
+    parser.add_argument('--new_data_dir', type=str, default = './data', help='directory where to put the new data')
     args = parser.parse_args()
     data_dir = args.data_dir
-    
+    new_data_dir = args.new_data_dir
 
-    build_mfccs(data_dir,train=True)
-    build_mfccs(data_dir, train=False)
+    build_mfccs(data_dir, new_data_dir, train=True)
+    build_mfccs(data_dir, new_data_dir, train=False)
