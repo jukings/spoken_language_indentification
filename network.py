713737c839fb3b6ed6a4ed4a9e654ddc467807ef
jukings <salvador.julien@outlook.com>
2022-09-22 20:58:54 +0200

    adding normalization

--- network.py
+++ network.py
@@ -21,31 +21,36 @@ class MFCCS_net(nn.Module):
         self.output_dim = output_dim
         self.input_dim = input_dim
 
-        self.conv1 = nn.Conv2d(in_channels=1,out_channels=32,kernel_size=7) 
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=32,kernel_size=7)
+        self.batch_normalization1 = BatchNorm2d(32)
         self.conv2 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=5, padding='same')
+        self.batch_normalization2 = BatchNorm2d(64)
         self.conv3 = nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3, padding='same')
+        self.batch_normalization3 = BatchNorm2d(128)
         self.conv4 = nn.Conv2d(in_channels=128,out_channels=256,kernel_size=3, padding='same')
+        self.batch_normalization4 = BatchNorm2d(256)
         self.conv5 = nn.Conv2d(in_channels=256,out_channels=512,kernel_size=3, padding='same')
+        self.batch_normalization5 = BatchNorm2d(512)
         self.fc1 = Linear(512*1*14,256)
         self.fc2 = Linear(256,self.output_dim)
-        self.dropout1 = Dropout(p=0.2)
-        self.dropout2 = Dropout(p=0.5)
+        
+        self.dropout = Dropout(p=0.5)
 
     def forward(self, x, mode=1):
 
         x = F.relu(F.max_pool2d(self.conv1(x),kernel_size=2, stride=2, padding=1))
-        x = self.dropout1(x)
+        x = self.batch_normalization1(x)
         x = F.relu(F.max_pool2d(self.conv2(x),kernel_size=3, stride=2, padding=1))
-        x = self.dropout1(x)
+        x = self.batch_normalization2(x)
         x = F.relu(F.max_pool2d(self.conv3(x),kernel_size=3, stride=2, padding=1))
-        x = self.dropout1(x)
+        x = self.batch_normalization3(x)
         x = F.relu(F.max_pool2d(self.conv4(x),kernel_size=3, stride=2, padding=1))
-        x = self.dropout1(x)
+        x = self.batch_normalization4(x)
         x = F.relu(F.max_pool2d(self.conv5(x),kernel_size=3, stride=2, padding=1))
+        x = self.batch_normalization5(x)
         x = flatten(x,mode)
-        x = self.dropout1(x)
         x = F.relu(self.fc1(x))
-        x = self.dropout2(x)
+        x = self.dropout(x)
         x = F.softmax(self.fc2(x), dim=1)
         
         return x
